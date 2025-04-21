9e001eb822adb3044e4b416d3b89043003d03686
Arsenij Pisarevskij <a_sarpi@mail.ru>
2025-04-21 19:40:06 +0300

    E: f2 – add timestamp logging

--- src/hello.sh
+++ src/hello.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-if test "$argv[1]" = "--green"
+if [ "$1" = "--green" ]; then
   echo -e "\e[32mHello, Feature 1!\e[0m"
 else
   echo "Hello, Feature 1!"
-end
+fi
+echo "$(date): run" >> run.log
